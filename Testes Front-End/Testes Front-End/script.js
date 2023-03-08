9ae65faa33889016b03b2f19de927ca9dc93361e
Analuz Santos <108814723+AnaluzSantos@users.noreply.github.com>
2023-03-08 12:39:14 -0300

    Delete Testes Front-End directory

--- Testes Front-End/Testes Front-End/script.js
+++
@@ -1,8 +0,0 @@
-// Crie um odal usando HTML, CSS e JavaScript
-function abrirModal(){
-    document.getElementById('modal').style.display = 'flex'
-}
-function fecharModal(){
-    document.getElementById('modal').style.display = 'none'
-}
-console.log(fecharModal())
