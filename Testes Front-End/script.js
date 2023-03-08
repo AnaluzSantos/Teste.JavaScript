aadc576bfdacd0957bd7b032a88343fbf6a5e5f6
Analuz Santos <108814723+AnaluzSantos@users.noreply.github.com>
2023-03-08 12:41:39 -0300

    Add files via upload

---
+++ Testes Front-End/script.js
@@ -0,0 +1,8 @@
+// Crie um odal usando HTML, CSS e JavaScript
+function abrirModal(){
+    document.getElementById('modal').style.display = 'flex'
+}
+function fecharModal(){
+    document.getElementById('modal').style.display = 'none'
+}
+console.log(fecharModal())
